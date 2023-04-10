74e7e5bc30865539123d21040c6f9d4c665440c6
Kohei-mana <kohei.manabe@tokyo.supersoftware.co.jp>
2023-04-10 18:24:28 +0900

    PHP果物課題

--- main.php
+++ main.php
@@ -4,25 +4,25 @@
     // echo $peach1->name . '<br/>';
     // echo $peach1->price;
     $pArray = [];
-    for($i =0; $i <= 15; $i++){
+    for($i =0; $i <= 14; $i++){
         $peach[$i] = new Peach();
         array_push($pArray, $peach[$i]->price);
         echo '<br/>';
-        echo 'もも'. $i . ' 価格：'. $pArray[$i];
+        echo 'もも' . $i+1 . ' 価格：'. $pArray[$i];
     }
     echo '<br/>';
-    echo '<br/>'.'最高値：' . max($pArray);
-    echo '<br/>'.'最低値：' . min($pArray);
-    echo '<br/>'.'平均価格：' . array_sum($pArray)/count($pArray);
+    echo '<br/>'. '最高値：' . max($pArray);
+    echo '<br/>'. '最低値：' . min($pArray);
+    echo '<br/>'. '平均価格：' . array_sum($pArray)/count($pArray);
     echo '<br/>';
 
     require_once("strawberry.php");
     $sArray = [];
-    for($i =0; $i <= 15; $i++){
+    for($i =0; $i <= 14; $i++){
         $strawberry[$i] = new Strawberry();
         array_push($sArray, $strawberry[$i]->price);
         echo '<br/>';
-        echo 'いちご'. $i . ' 価格：'. $sArray[$i];
+        echo 'いちご'. $i+1 . ' 価格：'. $sArray[$i];
     }
     echo '<br/>';
     echo '<br/>'.'最高値：' . max($sArray);
